939437894adcd8a9d6ee1590191ddb68769550e8
evwilkin <evan.wilkinson@gmail.com>
2020-03-19 17:25:45 -0400

    strict function types on contextselector and datatoolbar

--- packages/react-integration/demo-app-ts/src/components/demos/DataToolbarDemo/DataToolbarDemo.tsx
+++ packages/react-integration/demo-app-ts/src/components/demos/DataToolbarDemo/DataToolbarDemo.tsx
@@ -4,6 +4,8 @@ import {
   ButtonVariant,
   DataToolbar,
   DataToolbarItem,
+  DataToolbarChip,
+  DataToolbarChipGroup,
   DataToolbarContent,
   DataToolbarFilter,
   DataToolbarToggleGroup,
@@ -96,8 +98,9 @@ export class DataToolbarDemo extends React.Component<DataToolbarProps, DataToolb
     this.onSelect('risk', event, selection);
   };
 
-  onDelete = (type = '', id = '') => {
+  onDelete = (type: string | DataToolbarChipGroup = '', id: DataToolbarChip | string = '') => {
     if (type) {
+      const lowercaseType = typeof type === 'string' ? type.toLowerCase() : type.name.toLowerCase();
       this.setState(prevState => {
         const newState = Object.assign(prevState);
         newState.filters[type.toLowerCase()] = newState.filters[type.toLowerCase()].filter((s: string) => s !== id);
