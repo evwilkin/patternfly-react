8f9dccb6b76e008b94b0995229575c5baf76dacb
evwilkin <evan.wilkinson@gmail.com>
2021-04-20 11:02:04 -0400

    updated Toolbar resize function

--- packages/react-core/src/components/Toolbar/Toolbar.tsx
+++ packages/react-core/src/components/Toolbar/Toolbar.tsx
@@ -42,6 +42,7 @@ export interface ToolbarState {
   isManagedToggleExpanded: boolean;
   /** Object managing information about how many chips are in each chip group */
   filterInfo: FilterInfo;
+  windowWidth: number;
 }
 
 interface FilterInfo {
@@ -54,7 +55,8 @@ export class Toolbar extends React.Component<ToolbarProps, ToolbarState> {
   staticFilterInfo = {};
   state = {
     isManagedToggleExpanded: false,
-    filterInfo: {}
+    filterInfo: {},
+    windowWidth: window.innerWidth
   };
 
   isToggleManaged = () => !(this.props.isExpanded || !!this.props.toggleIsExpanded);
@@ -65,10 +67,13 @@ export class Toolbar extends React.Component<ToolbarProps, ToolbarState> {
     }));
   };
 
-  closeExpandableContent = () => {
-    this.setState(() => ({
-      isManagedToggleExpanded: false
-    }));
+  closeExpandableContent = (e: any) => {
+    if (e.target.innerWidth !== this.state.windowWidth) {
+      this.setState(() => ({
+        isManagedToggleExpanded: false,
+        windowWidth: e.target.innerWidth
+      }));
+    }
   };
 
   componentDidMount() {
