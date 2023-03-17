4158704f685f3ca5a773c0f75f77c8bd9a5e8240
Evan <evan.wilkinson@redhat.com>
2023-03-17 11:41:08 -0400

    added chatgpt-4 file descriptions

--- packages/react-styles/scripts/copyStyles.js
+++ packages/react-styles/scripts/copyStyles.js
@@ -1,3 +1,15 @@
+/*
+This code uses the fs-extra and path modules to copy the contents of the assets/images directory in the PatternFly package to a css/assets/images directory relative to the current script's location.
+
+Specifically, the code does the following:
+
+1 - It defines two variables, toDir and fromDir, that specify the destination directory and source directory, respectively. toDir is set to a resolved absolute path of the ../css directory relative to the current script's directory using the __dirname and resolve functions from the path module. fromDir is set to the resolved absolute path of the directory containing the package.json file of the @patternfly/patternfly package, which is found using require.resolve and dirname.
+
+2 - It uses the copySync function from the fs-extra module to copy the contents of the assets/images directory in fromDir to the css/assets/images directory in toDir. The join function from the path module is used to create the correct paths for the source and destination directories.
+
+Overall, this code is used to copy the image assets used in the PatternFly package to the css/assets/images directory in preparation for building the CSS-in-JS files.
+*/
+
 const { copySync } = require('fs-extra');
 const { resolve, dirname, join } = require('path');
 
--- packages/react-styles/scripts/generateClassMaps.js
+++ packages/react-styles/scripts/generateClassMaps.js
@@ -1,7 +1,37 @@
+/*
+This code exports a single function called generateClassMaps which generates a map of CSS class names to their corresponding CSS selectors by reading and parsing CSS files. The output is a map of file names to classMaps.
+
+The function works in the following way:
+
+It requires and initializes some external modules: path, fs-extra, glob, and camel-case.
+
+It defines a function called getCSSClasses that takes a CSS string as input and returns an array of all the CSS class names present in the string.
+
+It defines a function called formatClassName that takes a CSS class name as input, removes any leading .pf- prefix, and converts the remaining name to camelCase.
+
+It defines a function called isModifier that takes a CSS class name as input and returns a boolean indicating whether the class name starts with the .pf-m- prefix.
+
+It defines a function called getClassMaps that takes a CSS string as input, extracts all the distinct CSS class names present in the string, and returns a map of formatted CSS class names to their corresponding CSS selectors. It calls the formatClassName and isModifier functions to transform each class name appropriately.
+
+It defines the main generateClassMaps function that does the following:
+a. It finds the path to the patternfly.css file using require.resolve() and path.dirname().
+b. It searches for all .css files in the patternfly.css directory using glob.sync().
+c. It excludes certain files from the search using the ignore option.
+d. It also searches for all .css files in the src/css directory.
+e. It reads the content of each file, calls the getClassMaps function on the CSS string, and adds the resulting class map to a map of file names to class maps.
+f. It returns the final map of file names to class maps.
+
+Overall, this code provides a utility for extracting and mapping CSS class names and selectors from CSS files, which could be useful for various purposes such as dynamic styling or generating documentation.
+*/
+
+
+
 const path = require('path');
 const fs = require('fs-extra');
 const glob = require('glob');
 const camelcase = require('camel-case');
+
+const pfVersion = 5;
 
 /**
  * @param {string} cssString - CSS string
@@ -33,7 +63,7 @@ function getClassMaps(cssString) {
 
   distinctClasses.forEach(className => {
     const key = formatClassName(className);
-    const value = className.replace('.', '').trim();
+    const value = className.replace('.', '').trim().concat(`-v${pfVersion}`);
     if (isModifier(className)) {
       res.modifiers = res.modifiers || {};
       res.modifiers[key] = value;
