5ec0e4a8c451cffb71ef2d4d7c371285e48c2b1d
Evan <evan.wilkinson@redhat.com>
2024-05-17 11:02:18 -0400

    chore(dev): list version numbers on landing page

--- packages/react-docs/patternfly-docs/pages/index.js
+++ packages/react-docs/patternfly-docs/pages/index.js
@@ -1,5 +1,13 @@
 import React from 'react';
-import { Title, PageSection } from '@patternfly/react-core';
+import {
+  Title,
+  PageSection,
+  DescriptionList,
+  DescriptionListTerm,
+  DescriptionListGroup,
+  DescriptionListDescription
+} from '@patternfly/react-core';
+import packageJson from '../../package.json';
 
 // https://philipwalton.github.io/solved-by-flexbox/demos/vertical-centering/
 const centerStyle = {
@@ -15,17 +23,31 @@ const IndexPage = () => {
     url: process.env.prurl
   };
 
+  const allDependencies = { ...packageJson.dependencies, ...packageJson.devDependencies };
+
   return (
     <PageSection variant="light" style={centerStyle}>
       <div style={{ flex: 'none', textAlign: 'center' }}>
         <Title size="4xl" headingLevel="h1">
-          PatternFly 4 React Docs
+          PatternFly React Docs
         </Title>
         <Title size="2xl" headingLevel="h2">
           {prInfo.num ? <a href={prInfo.url}>PR #{prInfo.num}</a> : 'Hi people!'}
         </Title>
-        <p>Welcome to Patternfly 4 React docs.</p>
+        <p>Welcome to Patternfly React docs.</p>
         <p>Now go build something great.</p>
+        <hr />
+        <div style={{ textAlign: 'start' }}>
+          <p>You are currently running:</p>
+          <DescriptionList isHorizontal isAutoFit>
+            {Object.keys(allDependencies).map((key) => (
+              <DescriptionListGroup key={key}>
+                <DescriptionListTerm>{key}</DescriptionListTerm>
+                <DescriptionListDescription>{allDependencies[key]}</DescriptionListDescription>
+              </DescriptionListGroup>
+            ))}
+          </DescriptionList>
+        </div>
       </div>
     </PageSection>
   );
